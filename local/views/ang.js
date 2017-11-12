987f101a181d644dc0a35b826c26e9c15e30ae3c
Arshad Rizwan Kareem <arshadrizwan96@gmail.com>
2017-11-12 16:19:44 +0530

    Multi language
    Added multi language compatable

--- local/views/ang.js
+++ local/views/ang.js
@@ -2,7 +2,9 @@
 var app = angular.module("compiler",['ngSanitize']);
 
 app.controller('result',  function($scope,$http){
+	$scope.lanarray=["c","cpp","java","python","perl","php","ruby","csharp","mysql","oracle","haskell","clojure","bash","scala","erlang","lua","javascript","go","d","ocaml","r","pascal","sbcl","python3","groovy","objectivec","fsharp","cobol","visualbasic","lolcode","smalltalk","tcl","whitespace","tsql","java8","db2","octave","xquery","racket","rust","swift","fortran"];
 	$scope.output="";
+	var lanjson={"c":1,"cpp":2,"java":3,"python":5,"perl":6,"php":7,"ruby":8,"csharp":9,"mysql":10,"oracle":11,"haskell":12,"clojure":13,"bash":14,"scala":15,"erlang":16,"lua":18,"javascript":20,"go":21,"d":22,"ocaml":23,"r":24,"pascal":25,"sbcl":26,"python3":30,"groovy":31,"objectivec":32,"fsharp":33,"cobol":36,"visualbasic":37,"lolcode":38,"smalltalk":39,"tcl":40,"whitespace":41,"tsql":42,"java8":43,"db2":44,"octave":46,"xquery":48,"racket":49,"rust":50,"swift":51,"fortran":54};
 	$scope.run=function() {
 		
 		var edv=editor.getValue();
@@ -29,7 +31,9 @@ app.controller('result',  function($scope,$http){
 		var edv=editor.getValue();
 	$scope.compilemsg="";
 	$scope.output="Compiling....";
-	var params={name:{ source:edv,lang: 5,testcases: '["Hello World"]'}};
+	var lan=$scope.lang;
+	var num=lanjson[lan];
+	var params={name:{ source:edv,lang: num,testcases: '["Hello World"]'}};
 	$http.post('http://localhost:3000/compile',JSON.stringify(params)).success(function(response){
 		console.log(response);
 		$scope.compilemsg=response.compilemsg;
